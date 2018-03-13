8b1d987feb0b5fbc06bd9b616197a5dad1c2e351
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-14 08:33:14 +0900

    Add setup-opam-cross-windows.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,19 +3,6 @@ set -uexv
 
 . .travis/common.sh
 
-opam repository -y add windows ./opam-cross-windows
-opam install -y ocaml-windows
-
-cat >$HOME/.opam/$OCAMLVER/windows-sysroot/bin/ocamlmklib <<EOD
-#!/bin/sh
-$HOME/.opam/$OCAMLVER/bin/ocamlrun "$HOME/.opam/$OCAMLVER/windows-sysroot/bin/ocamlmklib.exe" -L/usr/local/$ARCH/lib "\$@"
-EOD
-
-# export LDFLAGS="-L$HOME/$ARCH/lib"
-# export CPPFLAGS="-I$HOME/$ARCH/include"
-export LDFLAGS=""
-export CPPFLAGS=""
-
 cd SATySFi
 patch -p1 < ../files/patches/otfm-dev-null-windows.patch
 patch -p1 < ../files/patches/win32-userprofile.patch
