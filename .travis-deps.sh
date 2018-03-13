ea5d1af43ffbc67e0e5acec732e33196841a935a
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-13 16:19:41 +0900

    Update deps.

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -2,17 +2,14 @@
 set -ue
 
 if [[ "$SATYSFI_TARGET" = "windows32" ]]; then
-  ARCH=i686-w64-mingw32
+  ARCH="i686-w64-mingw32"
+  OCAMLVER="4.06.1+32bit"
 elif [[ "$SATYSFI_TARGET" = "windows64" ]]; then
-  ARCH=x86_64-w64-mingw32
+  ARCH="x86_64-w64-mingw32"
+  OCAMLVER="4.06.1"
 else
   echo "Error: unknown \$SATYSFI_TARGET = $SATYSFI_TARGET" >&2
 fi
 
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s $HOME/bin
-if [[ "$SATYSFI_TARGET" = "windows32" ]]; then
-  opam switch "4.06.1+32bit"
-else
-  opam switch "4.06.1"
-fi
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s $HOME/bin $OCAMLVER
 opam update
