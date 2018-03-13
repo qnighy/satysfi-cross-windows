4775ec3cabeba1054a98697d379e3a5b00003857
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-13 21:55:58 +0900

    Add shims for ld too.

--- .travis/deps.sh
+++ .travis/deps.sh
@@ -15,7 +15,12 @@ cat >$HOME/bin/$ARCH-gcc <<EOD
 #!/bin/sh
 /usr/bin/$ARCH-gcc -L/usr/local/$ARCH/lib -I/usr/local/$ARCH/include "\$@"
 EOD
+cat >$HOME/bin/$ARCH-ld <<EOD
+#!/bin/sh
+/usr/bin/$ARCH-ld -L/usr/local/$ARCH/lib "\$@"
+EOD
 chmod +x $HOME/bin/$ARCH-gcc
+chmod +x $HOME/bin/$ARCH-ld
 
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s $HOME/bin $OCAMLVER
 cat >> ~/.ocamlinit <<EOD
