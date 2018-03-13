d1a4a37e6bb03a8b14b66a61bb479392846a60e5
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-14 08:49:37 +0900

    Yes auto-setup please

--- .travis/setup-opam.sh
+++ .travis/setup-opam.sh
@@ -4,14 +4,7 @@ set -uexv
 . .travis/common.sh
 
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin $OCAMLVER
-/usr/local/bin/opam init --comp $OCAMLVER
-cat >> ~/.ocamlinit <<EOD
-let () =
-  try Topdirs.dir_directory (Sys.getenv "OCAML_TOPLEVEL_PATH")
-  with Not_found -> ()
-;;
-EOD
+/usr/local/bin/opam init --auto-setup --compiler=$OCAMLVER
 eval `opam config env`
-echo '. "$HOME/.opam/opam-init/init.sh" > /dev/null 2> /dev/null || true' >> ~/.bash_profile
 
 opam update
