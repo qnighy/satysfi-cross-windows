9026bc375c19fd0a2a4ed11c9227a5c2f08780b4
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-14 08:27:18 +0900

    Add setup-mxe.

---
+++ .travis/setup-mxe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -uexv
+
+. .travis/common.sh
+
+echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee \
+   /etc/apt/sources.list.d/mxeapt.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+sudo apt update
+sudo apt install mxe-{x86-64,i686}-w64-mingw32.static-{gcc,libpng,jpeg,freetype}
