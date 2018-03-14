2740f95f0946053187fe0a63ae850e073dc3323e
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-14 09:19:36 +0900

    Remove patches from ppx_deriving-windows.

--- opam-aux/packages/ppx_deriving-windows.4.2.1/files/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ue
-
-ocamlbuild -classic-display -use-ocamlfind src/ppx_deriving_main.native -plugin-tag 'package(cppo_ocamlbuild)'
-rm _build/src/*.cm*
-# ocamlbuild -use-ocamlfind -just-plugin -plugin-tag 'package(cppo_ocamlbuild)'
-OCAMLFIND_TOOLCHAIN=windows ocaml pkg/build.ml native=true native-dynlink=true
-sed -i.bak ppx_deriving.install -e '6a\
-  "_build/src/ppx_deriving_main.native" {"ppx_deriving_main.native"}'
