7474a38bedceeb3221f3e3ea8658bb66e5fc1f9d
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-14 10:36:01 +0900

    Use shared MXE builds instead of static.

--- .travis/setup-mxe.sh
+++ .travis/setup-mxe.sh
@@ -7,4 +7,4 @@ echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee \
    /etc/apt/sources.list.d/mxeapt.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 sudo apt update
-sudo apt install mxe-{x86-64,i686}-w64-mingw32.static-{gcc,libpng,jpeg,freetype}
+sudo apt install mxe-{x86-64,i686}-w64-mingw32.shared-{gcc,libpng,jpeg,freetype}
--- .travis/setup-opam-cross-windows.sh
+++ .travis/setup-opam-cross-windows.sh
@@ -7,9 +7,9 @@ opam repository -y add windows ./opam-cross-windows
 opam pin -y add topkg https://github.com/whitequark/topkg.git
 
 if [[ "$SATYSFI_TARGET" = "windows32" ]]; then
-  TOOLPREF32=/usr/lib/mxe/usr/bin/i686-w64-mingw32.static- opam install -y conf-gcc-windows32
+  TOOLPREF32=/usr/lib/mxe/usr/bin/i686-w64-mingw32.shared- opam install -y conf-gcc-windows32
 else
-  TOOLPREF64=/usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static- opam install -y conf-gcc-windows64
+  TOOLPREF64=/usr/lib/mxe/usr/bin/x86_64-w64-mingw32.shared- opam install -y conf-gcc-windows64
 fi
 
 opam install -y opam-installer
