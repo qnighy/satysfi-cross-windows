5238c5782f55c2e9094d479712023c06f7f367ec
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-14 23:33:55 +0900

    Include fonts in release artifacts.

--- .travis/build.sh
+++ .travis/build.sh
@@ -25,5 +25,18 @@ cp "/usr/lib/mxe/usr/$ARCH.shared/bin/libjpeg-9.dll" "$RELEASE_NAME/"
 cp "/usr/lib/mxe/usr/$ARCH.shared/bin/libgcc_s_$GCC_EXCEPTION_TYPE-1.dll" "$RELEASE_NAME/"
 
 cp -r licenses "$RELEASE_NAME/"
+cp -r SATySFi/lib-satysfi "$RELEASE_NAME/"
+rm "$RELEASE_NAME/lib-satysfi/dist/fonts/.gitkeep"
+cp \
+  /usr/share/fonts/opentype/ipaexfont-mincho/ipaexm.ttf \
+  /usr/share/fonts/opentype/ipaexfont-gothic/ipaexg.ttf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmroman10-regular.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmroman10-italic.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmmono10-regular.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmsans10-regular.otf \
+  /usr/share/fonts/truetype/junicode/Junicode.ttf \
+  /usr/share/fonts/truetype/junicode/Junicode-Italic.ttf \
+  /usr/share/texlive/texmf-dist/fonts/opentype/public/lm-math/latinmodern-math.otf \
+  "$RELEASE_NAME/lib-satysfi/dist/fonts/"
 
 zip -r "$RELEASE_NAME.zip" "$RELEASE_NAME"
