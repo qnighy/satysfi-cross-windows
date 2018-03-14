09b82aa76fd050efe10d6aa6a93416d9f13ca472
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-14 09:52:12 +0900

    Add deploy procedure.

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,3 +10,13 @@ cd ..
 
 opam repository -y add satysfi-aux ./opam-aux
 opam pin -y add satysfi-windows .
+
+if [[ "$SATYSFI_TARGET" = "windows32" ]]; then
+  mkdir satysfi32
+  cp $HOME/.opam/$OCAMLVER/windows-sysroot/bin/satysfi satysfi32/satysfi.exe
+  zip -r satysfi32.zip satysfi32
+else
+  mkdir satysfi64
+  cp $HOME/.opam/$OCAMLVER/windows-sysroot/bin/satysfi satysfi64/satysfi.exe
+  zip -r satysfi64.zip satysfi64
+fi
