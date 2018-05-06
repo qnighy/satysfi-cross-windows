79b03a205985cf34c867e2d1aa0405be30d0608b
Masaki Hara <ackie.h.gmai@gmail.com>
2018-05-06 17:01:09 +0900

    Upstream update 2018-04-02

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,9 +3,5 @@ set -uexv
 
 . .travis/common.sh
 
-cd SATySFi
-patch -p1 < ../files/patches/otfm-dev-null-windows.patch
-cd ..
-
 opam repository -y add satysfi-aux ./opam-aux
 opam pin -y add satysfi-windows .
