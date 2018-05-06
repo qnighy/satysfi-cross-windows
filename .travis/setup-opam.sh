262b7e9f5b9fad88434691cff755e183bba3af24
Masaki Hara <ackie.h.gmai@gmail.com>
2018-05-06 18:35:39 +0900

    Workaround for ocaml-windows32 bug.

--- .travis/setup-opam.sh
+++ .travis/setup-opam.sh
@@ -6,5 +6,7 @@ set -uexv
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin $OCAMLVER
 /usr/local/bin/opam init --auto-setup --compiler=$OCAMLVER
 eval `opam config env`
+# Workaround for ocaml-windows32
+mkdir -p "$(opam config var lib)/graphics"
 
 opam update
