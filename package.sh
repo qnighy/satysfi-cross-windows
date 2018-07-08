79e82d5553a4981f986f4d54c744ed98174a4b91
Masaki Hara <ackie.h.gmai@gmail.com>
2018-07-08 11:14:02 +0900

    Include more fonts.

--- package.sh
+++ package.sh
@@ -26,13 +26,18 @@ cp \
   /usr/share/fonts/opentype/ipaexfont-mincho/ipaexm.ttf \
   /usr/share/fonts/opentype/ipaexfont-gothic/ipaexg.ttf \
   /usr/share/texmf/fonts/opentype/public/lm/lmroman10-regular.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmroman10-bold.otf \
   /usr/share/texmf/fonts/opentype/public/lm/lmroman10-italic.otf \
   /usr/share/texmf/fonts/opentype/public/lm/lmmono10-regular.otf \
   /usr/share/texmf/fonts/opentype/public/lm/lmsans10-regular.otf \
   /usr/share/fonts/truetype/junicode/Junicode.ttf \
+  /usr/share/fonts/truetype/junicode/Junicode-Bold.ttf \
   /usr/share/fonts/truetype/junicode/Junicode-Italic.ttf \
   /usr/share/texlive/texmf-dist/fonts/opentype/public/lm-math/latinmodern-math.otf \
+  /usr/share/texlive/texmf-dist/fonts/opentype/public/Asana-Math/Asana-Math.otf \
   "$RELEASE_NAME/lib/dist/fonts/"
+
+mv "$RELEASE_NAME/lib/dist/fonts/Asana-Math.otf" "$RELEASE_NAME/lib/dist/fonts/Asana-math.otf"
 
 cp -r SATySFi/demo "$RELEASE_NAME/"
 
